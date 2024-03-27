ea769c6dd6240f40196e3085f8db59c153f769a6
Ambroise <windox.condar@gmail.com>
2024-03-27 12:19:02 +0100

    ajout : calcul, delete all, popup, responsive

--- SRC/panier-script.js
+++ SRC/panier-script.js
@@ -1,38 +1,45 @@
+
 const stock = {
 
     item1 : {
         id:0,
         name: "captor",
         price: 200,
-        qty : 1
+        qty : 1,
+        newprice: 0
     },
 
     item2 : {
         id:0,
         name: "cable",
         price: 2.50,
-        qty : 1
+        qty : 1,
+        newprice: 0
     },
 
     item3: {
         id:0,
         name: "EVH",
         price: 749,
-        qty : 1
+        qty : 1,
+        newprice: 0
     },
 
 
 };
-    
-document.getElementById("item1").innerHTML = stock.item1.price + "€"; //charge tout les prix, inutile pour l'exo mais pratique dans d'autre cas
-document.getElementById("item2").innerHTML = stock.item2.price + "€";
-document.getElementById("item3").innerHTML = stock.item3.price + "€";
 
 
-handleStock(); //on apelle une premiere fois pour charcher tout les stock
 
 
-function handleStock(){
+InnitStock(); //on apelle une premiere fois pour charcher tout les stock
+
+
+function InnitStock(){
+
+    document.getElementById("item1").innerHTML = stock.item1.price + "€"; //charge tout les prix
+    document.getElementById("item2").innerHTML = stock.item2.price + "€";
+    document.getElementById("item3").innerHTML = stock.item3.price + "€";
+
     document.getElementById("qty1").innerText = stock.item1.qty;
     document.getElementById("qty2").innerText = stock.item2.qty;
     document.getElementById("qty3").innerText = stock.item3.qty;
@@ -41,9 +48,84 @@ function handleStock(){
 
 
 function handleQTY(itemId, change) {
+    
+    
     const item = stock[itemId]; // Obtenez l'objet de stock correspondant à l'ID de l'article
-    if (item) {
+
+    const qtyitem = "qty" + itemId.slice(-1);
+    const priceitem = "item" + itemId.slice(-1);
+
+    document.querySelector(".add").disabled = true; //desactive le bouton le temps que les donnée sont traité
+
+    if (item.qty-1 >= 0) { //est-ce que l'article est positif ?
+        console.log(item.qty);
         item.qty += change; // Mettez à jour la quantité en ajoutant ou en soustrayant le changement spécifié
-        document.getElementById("qty" + itemId.slice(-1)).innerHTML = item.qty; // modifie l'element qty, prends le mot et ajoute le slice du dernier characters de itemid créant par ex qty1
+        item.newprice = item.qty * item.price;
+
+        document.getElementById(qtyitem).innerHTML = item.qty;
+        item.newprice = item.qty * item.price;
+        
+
+
     }
+    else if (item.qty <= 0) { //else le forcer a etre a positif
+        item.qty = 1;
+        document.getElementById(qtyitem).innerHTML = item.qty;
+        item.newprice = item.qty * item.price;
+        
+    }
+
+    document.querySelector(".add").disabled = false;
+}
+
+function handleCheckout() {
+    let price = 0;
+    for(const key in stock) {
+       if(Object.hasOwnProperty.call(stock, key)) { //si dans l'object il y a props (stock, key) alors:
+        const item = stock[key];
+
+        price = price + item.qty * item.price; //multiplie le total existant + qty * price
+
+
+       }
+           
+    }
+    document.getElementById("totalPrice").innerHTML = "Total : " + price + "€";
+}
+
+
+function popshow(){
+    var popup = document.querySelector(".popup-container");
+    var page = document.querySelector(".container");
+    popup.style.visibility = "visible"; // Correction ici
+    page.style.filter = "blur(5px)";
+}
+
+function handleDeleteAll(erase){
+    var popup = document.querySelector(".popup-container");
+    var page = document.querySelector(".container");
+    if(erase)
+    {
+        for(const key in stock) {
+            if(Object.hasOwnProperty.call(stock, key)) {
+                const item = stock[key];
+                item.qty = 0;
+                InnitStock();
+                document.getElementById("totalPrice").innerHTML = "Total : 0";
+
+                popup.style.visibility = "hidden"; 
+                page.style.filter = "blur(0px)";
+            }
+    
+            
+        }
+
+
+    }
+    else{
+
+        popup.style.visibility = "hidden"; 
+        page.style.filter = "blur(0px)";
+    }
+    
 }
