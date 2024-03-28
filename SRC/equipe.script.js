6eb176844f70383d9392ee15e0353a6ebc21bf79
Daabla <pessegueiro.e@gmail.com>
2024-03-28 16:15:02 +0100

    amélioration du js

--- SRC/equipe.script.js
+++ SRC/equipe.script.js
@@ -19,22 +19,20 @@ const ctaEdit = document.getElementById("cta-form-edit"); // Le btn "modifier" d
 const ctaCancel = document.getElementById("cta-form-cancel"); // Le btn "annuler" du formulaire
 
 
-
 // Formulaire inputs
 let nameForm = document.getElementById("name-form");
 let posteForm = document.getElementById("poste-form");
 let texteAreaForm = document.getElementById("textarea-form");
-
 let imageForm = document.getElementById("image-profil");
 
 
-let currentCard = null 
+let currentCard = 0 
 
 
 // Fonction permettant de récupérer les données de la cards pour les innsérer dans le formulaire
 function showFormWithInfo(n, p, d) {
     
-    currentCard = document.getElementById(n).closest('.card'); // Stocke la carte actuellement éditée
+        currentCard = document.getElementById(n).closest('.card'); // Stocke la carte actuellement éditée qui est la plus proche du bouton utilisé
 
     //Recupération des infos de la card
         const nomEmploye  = document.getElementById(n).textContent;
@@ -43,10 +41,10 @@ function showFormWithInfo(n, p, d) {
         const imageEmploye = currentCard.querySelector('.card-img').src;
 
     // Insertion des valeurs dans le formulaire
-    nameForm.value = nomEmploye;
-    posteForm.value = posteEmploye;
-    imageForm.value = imageEmploye;
-    texteAreaForm.value = descriptionEmploye;
+        nameForm.value = nomEmploye;
+        posteForm.value = posteEmploye;
+        imageForm.value = imageEmploye;
+        texteAreaForm.value = descriptionEmploye;
 
     formDisplay();
 };
@@ -76,9 +74,6 @@ editIcon3.addEventListener('click', function(event) {  // ouvre le formulaire au
     event.preventDefault(); // Pour empêcher tout comportement par défaut
     showFormWithInfo("nom3", "poste3", "description3");
 });
-
-
-
 
 
 ctaCancel.addEventListener('click', function() {
